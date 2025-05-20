1f24f7b088ac45789d3746e0112211a2110b223b
zoeznm <bomikim046@gmail.com>
2025-05-21 01:02:01 +0900

    라우팅 넣기

--- kbm/src/components/Navbar.tsx
+++ kbm/src/components/Navbar.tsx
@@ -1,19 +1,13 @@
-import Link from "next/link";
+import Link from 'next/link';
 import '../styles/Navbar.css';
 
 export default function Navbar() {
   return (
     <nav className="navbar">
       <ul>
-        <li>
-          <Link href="/bomi">About Bomi</Link>
-        </li>
-        <li>
-          <Link href="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
+        <li><Link href="/bomi">About Bomi</Link></li>
+        <li><Link href="/projects">Projects</Link></li>
+        <li><Link href="/contact">Contact</Link></li>
       </ul>
     </nav>
   );
