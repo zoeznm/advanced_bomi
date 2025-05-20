6247ff97d87998bada18f5575d45a2eb49c679e0
zoeznm <bomikim046@gmail.com>
2025-05-21 00:37:44 +0900

    필요한 파일 생성하고 루트 리디렉트 설정

--- kbm/next.config.ts
+++ kbm/next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+// File: next.config.ts
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/mainpage',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
